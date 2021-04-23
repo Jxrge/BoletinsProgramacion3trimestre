f94e41ffe133165beb645cd9514f803b691ccf46
Jxrge <72072830+Jxrge@users.noreply.github.com>
2021-04-23 21:45:31 +0200

    Delete Boletin20/src directory

--- Boletin20/src/Main.java
+++
@@ -1,9 +0,0 @@
-package com.jorgeboletin20;
-
-public class Main {
-    public static void main(String[] args) {
-        Componentes componentes = new Componentes();
-        componentes.iniciointerfaz();
-
-    }
-}
