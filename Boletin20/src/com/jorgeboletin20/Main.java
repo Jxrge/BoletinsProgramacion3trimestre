9deb43d371d8a1a35fab93f5ffb3fb1ae33ed073
Jxrge <72072830+Jxrge@users.noreply.github.com>
2021-04-23 21:44:40 +0200

    Add files via upload

---
+++ Boletin20/src/com/jorgeboletin20/Main.java
@@ -0,0 +1,9 @@
+package com.jorgeboletin20;
+
+public class Main {
+    public static void main(String[] args) {
+        Componentes componentes = new Componentes();
+        componentes.iniciointerfaz();
+
+    }
+}
